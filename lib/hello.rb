69027886eb50b586db73e4329c21e3d485cb88fd
Peter Rose <peterroseq1@gmail.com>
2020-03-10 16:21:05 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,32 +1,4 @@
 
-
-# call your method here!
-def yielding_with_arguments(num)
-  puts "the program is executing the code inside the method"
-  yield(num)
-  puts "now we are back in the method"
-end
-yielding_with_arguments(2) {|i| puts i * 2}
-
-# Code-Along:Building our own method with yield 
-["Tim", "Tom", "Jim"].each do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-  
-# Step 1: Defining our method to accept an argument 
-def hello_t(array)
-  # code here
-end
-# Step 2: Enacting an iteration 
-def hello_t(array)
-  i = 0
- 
-  while i < array.length
-    i = i + 1
-  end
-end
-# Step 3: Yielding to the block 
 def hello_t(array)
   i = 0 
   
@@ -34,6 +6,7 @@ def hello_t(array)
     yield array[i]
     i = i + 1 
   end
+  array
 end 
 
 
