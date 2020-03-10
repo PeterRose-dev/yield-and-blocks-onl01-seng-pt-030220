48da0b01b146df04a5de1cd531a6d0ab42074b3f
Peter Rose <peterroseq1@gmail.com>
2020-03-10 09:47:56 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -13,5 +13,7 @@ yielding_with_arguments(2) {|i| puts i * 2}
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
+# Step 1: Defining our method to accept an argument 
+def hello_t(array)
+  # code here
 end
-
