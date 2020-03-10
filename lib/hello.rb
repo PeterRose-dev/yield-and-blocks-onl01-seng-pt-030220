7cc6552dbaf6e18d1c578e3ee8b77eee4be49f1e
Peter Rose <peterroseq1@gmail.com>
2020-03-10 09:53:36 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -17,3 +17,28 @@ yielding_with_arguments(2) {|i| puts i * 2}
 def hello_t(array)
   # code here
 end
+# Step 2: Enacting an iteration 
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    i = i + 1
+  end
+end
+# Step 3: Yielding to the block 
+def hello_t(array)
+  i = 0 
+  
+  while i < array.length
+    yield array[i]
+    i = i + 1 
+  end
+end 
+# Step 4 : Calling our method 
+["Tim","Tom","Jim"]
+
+hello_t(["Tim","Tom","Jim"]) do |name|
+  if name.star_with("T")
+    puts "Hi, #{name}"
+  end
+end
