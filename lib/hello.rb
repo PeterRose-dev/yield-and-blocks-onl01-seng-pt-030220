6c9bbef7fd00e5b2b40f9240c29e84ac6c4e4670
Peter Rose <peterroseq1@gmail.com>
2020-03-10 09:37:47 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,9 @@
 
 
 # call your method here!
-def hello_t(["Tim", "Tom", "Jim"]) do |name|
+def hello_t(array) 
+  array = ["Tim", "Tom", "Jim"]
+  array do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
